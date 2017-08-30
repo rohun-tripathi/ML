5b3a16aba1bc9a9876ed4e0019038241be5c3de9
AlexKH22 <alexkh2249@gmail.com>
2017-08-30 14:01:39 -0400

    LSTM+CNN

---
+++ NN/Embedding.py
@@ -0,0 +1,16 @@
+from keras.models import Sequential
+from keras.layers import Embedding
+import numpy as np
+
+model = Sequential()
+model.add(Embedding(1000, 1, input_length=10))
+# the model will take as input an integer matrix of size (batch, input_length).
+# the largest integer (i.e. word index) in the input should be no larger than 999 (vocabulary size).
+# now model.output_shape == (None, 10, 64), where None is the batch dimension.
+
+input_array = np.random.randint(1000, size=(32, 10))
+
+model.compile('rmsprop', 'mse')
+output_array = model.predict(input_array)
+assert output_array.shape == (32, 10, 1)
+print(output_array)
